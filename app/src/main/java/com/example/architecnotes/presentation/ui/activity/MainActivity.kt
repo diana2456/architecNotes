a76f7f6a037db7b4c827f696fd6ff2bc0a5994c8
diana2456 <115098463+diana2456@users.noreply.github.com>
2023-04-25 17:34:10 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/architecnotes/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/architecnotes/presentation/ui/activity/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //added kotlin-dsl
+        //added app architecture
     }
 }
